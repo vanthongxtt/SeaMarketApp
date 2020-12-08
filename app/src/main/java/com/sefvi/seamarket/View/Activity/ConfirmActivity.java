d88e9f504879c6146728675c8ee3dbfcab388350
levuhuynh06112000 <huynh01633153596@gmail.com>
2020-12-08 10:41:30 +0700

    giao dien xac nhan don hàng

---
+++ app/src/main/java/com/sefvi/seamarket/View/Activity/ConfirmActivity.java
@@ -0,0 +1,16 @@
+package com.sefvi.seamarket.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sefvi.seamarket.R;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
